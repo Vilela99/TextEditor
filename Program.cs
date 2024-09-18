d715421ad7fc034bc0d360a925cbbb8cf461651d
Guilherme Vilela de Melo <vilela.meloguilherme@gmail.com>
2024-09-18 00:18:29 -0300

    Update 1.0 -  Text Editor

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.IO;
+
+namespace TextEditor
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("What do you want to do?");
+            Console.WriteLine("1 - Open file");
+            Console.WriteLine("2 - Edit file");
+            Console.WriteLine("0 - Exit");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Open(); break;
+                case 2: Edit(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Open()
+        {
+            Console.Clear();
+            Console.WriteLine("Inform the path of the file: ");
+            string path = Console.ReadLine();
+
+            using (var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
+
+        }
+
+        static void Edit()
+        {
+            Console.Clear();
+            Console.WriteLine("Type your text below: (ESC TO EXIT)");
+            Console.WriteLine("-----------------------");
+            string text = "";
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Save(text);
+        }
+
+        static void Save(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("Choose the name of the file");
+            var path = Console.ReadLine();
+
+            using (var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
+
+            Console.WriteLine($"File sucessfuly saved on {path}!");
+            Console.ReadLine();
+            Menu();
+        }
+
+    }
+}
